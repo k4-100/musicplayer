35f0bb2461ba3fdb9c9d586f8b994cf13e0f8e2c
Krystian Murzyn <krystianmurzynkontakt@gmail.com>
2021-05-10 17:09:54 +0200

    rewritten how the audio is played and loaded

--- player.js
+++ player.js
@@ -4,25 +4,79 @@ const audio = document.querySelector('#audio');
 // buttons
 const playBtn = document.querySelector('#play');
 
+// inner progress bar
+const innerProgressBar = document.querySelector('.inner-progress-bar');
 
-let isAudioPlaying = false; 
-/** @summary control whether audio has to play or not */
-function controlAudioPlay(){
-    if(!isAudioPlaying){
-        playBtn.classList.remove('play-btn');
-        playBtn.classList.add('pause-btn');
-        audio.play();
-        
-    }else{
-        playBtn.classList.remove('pause-btn');
-        playBtn.classList.add('play-btn');
-        audio.pause();
+console.log(innerProgressBar);
+// other variables
+let isAudioPlaying = true;
+let secondsPassed = 1; 
+let intervalID = null;
+
+
+const songs = [
+    'jazzyfrenchy',
+    'anewbeginning',
+    'happyrock',
+    'littleidea',
+    'ukulele',
+    'creativeminds'  
+];
+
+function loadSong(song){
+    audio.src = `mp3/${song}.mp3`;
+
+}
+
+
+function playAudio(){
+    playBtn.classList.remove('play-btn');
+    playBtn.classList.add('pause-btn');
+    audio.play();
+}
+
+function pauseAudio(){
+    playBtn.classList.remove('pause-btn');
+    playBtn.classList.add('play-btn');
+    audio.pause();
+}
+
+function adjustProgressBar(){
+    if(!audio.paused){
+        const duration = audio.duration;
+        innerProgressBar.style.width = `calc( 100% * 
+            ${secondsPassed++ / duration}`;
     }
-    
-    isAudioPlaying = !isAudioPlaying;
+
 }
 
 
 
+
+function playOrPauseAudio(){
+    if(audio.paused){
+        playAudio();
+        intervalID = setInterval(adjustProgressBar,1000);
+    }
+    else{
+        pauseAudio();
+        clearInterval(intervalID);
+       
+    }
+        
+        console.log(audio.videoTracks);
+}
+
+
 // event listeners
-playBtn.addEventListener('click',controlAudioPlay);
+playBtn.addEventListener('click',playOrPauseAudio);
+
+
+
+
+
+/*###########################################
+EXECUTION
+###########################################*/
+
+loadSong(songs[5]);
