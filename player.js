c88166f1209a777c0e30cb156b6578474de6aa0f
Krystian Murzyn <krystianmurzynkontakt@gmail.com>
2021-05-10 15:03:20 +0200

    Implemented basic audio playing handling - user can play or pause audio at will

--- player.js
+++ player.js
@@ -5,13 +5,25 @@ const audio = document.querySelector('#audio');
 const playBtn = document.querySelector('#play');
 
 
-// play / pause functions
-function playAudio(){
-    playBtn.classList.remove('play-btn');
-    playBtn.classList.add('pause-btn');
-    audio.play();
+let isAudioPlaying = false; 
+/** @summary control whether audio has to play or not */
+function controlAudioPlay(){
+    if(!isAudioPlaying){
+        playBtn.classList.remove('play-btn');
+        playBtn.classList.add('pause-btn');
+        audio.play();
+        
+    }else{
+        playBtn.classList.remove('pause-btn');
+        playBtn.classList.add('play-btn');
+        audio.pause();
+    }
+    
+    isAudioPlaying = !isAudioPlaying;
 }
+
+function 
 
 
 // event listeners
-playBtn.addEventListener('click',playAudio);
+playBtn.addEventListener('click',controlAudioPlay);
