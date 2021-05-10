b5600eebdbb316ad9872c185717e195def423a4f
Krystian Murzyn <krystianmurzynkontakt@gmail.com>
2021-05-10 14:58:14 +0200

    Started implementing audio control

--- player.js
+++ player.js
@@ -1,1 +1,17 @@
-console.log('run');
+// audio
+const audio = document.querySelector('#audio');
+
+// buttons
+const playBtn = document.querySelector('#play');
+
+
+// play / pause functions
+function playAudio(){
+    playBtn.classList.remove('play-btn');
+    playBtn.classList.add('pause-btn');
+    audio.play();
+}
+
+
+// event listeners
+playBtn.addEventListener('click',playAudio);
